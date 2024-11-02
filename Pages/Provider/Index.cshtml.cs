4c762cd6833475e3f76622b85c5fe9819cfe7f69
oskarnaranjo123 <oscarelmono123@gmail.com>
2024-11-02 02:32:27 -0500

    Index Providers

---
+++ Pages/Provider/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Provider
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Providers> Provider { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            // condición para las categorias
+            if (_context.Providers != null)
+            {
+                Provider = await _context.Providers.ToListAsync();
+            }
+        }
+    }
+}
